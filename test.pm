862ae4fa0f6ca8091ce4ef1bd8702535fdc6c14e
Melvin McMastertonson <amckhome@tpg.com.au>
2012-02-01 09:17:27 +1100

    Documented test.pm.

--- test.pm
+++ test.pm
@@ -2,15 +2,21 @@
 use strict;
 use 5.010;
 use warnings;
-use T_B;
+use T_B; #use twitter backfill package
 
+#basic subroutine that just prints out the value of the text field in each status
+#this can be changed to do whatever you need with the retrieved data, called once
+#per status. see https://dev.twitter.com/docs/api/1/get/statuses/user_timeline for
+#a list of available fields.
 sub my_sub {
     my $status = shift;
     say $status->{text};
 }
 
-binmode STDOUT, ":utf8";
+binmode STDOUT, ":utf8"; #required for STDOUT of some non-english characters
 
+#connect to twitter API
+#requires your own Twitter API keys, freely available from the twitter dev site.
 my $x = T_B->new(
    consumer_key    => "",
    consumer_secret => "",
@@ -18,4 +24,8 @@ my $x = T_B->new(
    access_token_secret => ""
 );
 $x->connect();
+#
+
+#request backfill of tweets for user 'hambargler', with my_sub being called
+#to act on each individual status.
 $x->backfill('hambargler',\&my_sub);
